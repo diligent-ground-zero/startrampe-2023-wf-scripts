8ca621cbaaaa8869eb4faf799494f2969a5bb1b7
Niko Dovidija <niko.dovidija@gmail.com>
2023-12-28 20:50:53 +0100

    slider fin

--- src/scripts/home/index.js
+++ src/scripts/home/index.js
@@ -3,77 +3,108 @@ import { Manipulation } from 'swiper/modules';
 import 'swiper/css';
 
 export const initSwipers = () => {
-  let previousActiveSlideIndex = undefined;
+  if (window.matchMedia('(max-width: 992px)').matches) {
+    const wasWirTunSwiper = document.querySelector('section.section_was_wir_tun .swiper-container .swiper-wrapper');
+    let previousActiveElementIndex;
 
-  const swiper = new Swiper('.swiper-container', {
-    modules: [Manipulation],
-    slidesPerView: 'auto',
-    centeredSlides: true,
-    centeredSlidesBounds: true,
-    centerInsufficientSlides: true,
-    // allowTouchMove: false,
-    slideToClickedSlide: true,
-    breakpoints: {
-      420: {
-        initialSlide: 1,
-        slidesPerView: 1,
-        slidesPerGroup: 1,
-        // slidesPerView: 1,
-        // spaceBetween: 0,
-      },
-      767: {
-        initialSlide: 1,
-        slidesPerView: 2,
-        spaceBetween: 50,
-        slidesPerGroup: 1,
-        slidesOffsetAfter: 100,
-        slidesOffsetBefore: 100,
-      },
-      991: {
-        initialSlide: 1,
-        spaceBetween: 50,
-        slidesOffsetAfter: 100,
-        slidesOffsetBefore: 100,
-        slidesPerView: 2,
-        slidesPerGroup: 2,
-      },
-      1280: {
-        slidesPerView: 2,
-        slidesPerGroup: 2,
-      },
-      1440: {
-        slidesPerView: 3,
-        slidesPerGroup: 1,
-        slidesOffsetAfter: 250,
-      },
-    },
-  });
+    for (let i = 0; i < wasWirTunSwiper.children.length; i++) {
+      wasWirTunSwiper.children[i].addEventListener('click', function () {
+        let currentElement = wasWirTunSwiper.children[i];
+
+        if (previousActiveElementIndex === undefined) {
+          currentElement.dataset.open = true;
+        } else if (previousActiveElementIndex === i) {
+          if (currentElement.dataset.open) {
+            delete currentElement.dataset.open;
+          } else {
+            currentElement.dataset.open = true;
+          }
+        } else {
+          delete wasWirTunSwiper.children[previousActiveElementIndex].dataset.open;
+          currentElement.dataset.open = true;
+        }
+
+        previousActiveElementIndex = i;
+      });
+    }
+  }
 
   if (window.matchMedia('(min-width: 992px)').matches) {
+    let previousActiveSlideIndex = undefined;
+
+    const swiper = new Swiper('.swiper-container', {
+      modules: [Manipulation],
+      slidesPerView: 'auto',
+      centeredSlides: true,
+      centeredSlidesBounds: true,
+      centerInsufficientSlides: true,
+      allowTouchMove: true,
+      //slideToClickedSlide: true,
+      breakpoints: {
+        420: {
+          initialSlide: 1,
+          slidesPerView: 1,
+          slidesPerGroup: 1,
+          // slidesPerView: 1,
+          // spaceBetween: 0,
+        },
+        767: {
+          initialSlide: 1,
+          slidesPerView: 2,
+          spaceBetween: 50,
+          slidesPerGroup: 1,
+          slidesOffsetAfter: 100,
+          slidesOffsetBefore: 100,
+        },
+        991: {
+          initialSlide: 1,
+          spaceBetween: 50,
+          slidesOffsetAfter: 100,
+          slidesOffsetBefore: 100,
+          slidesPerView: 2.5,
+          slidesPerGroup: 1,
+        },
+        1280: {
+          slidesPerView: 2.5,
+          initialSlide: 1,
+          slidesPerGroup: 1,
+          spaceBetween: 25,
+          slidesOffsetAfter: 50,
+        },
+        1440: {
+          slidesPerView: 3,
+          slidesPerGroup: 1,
+          slidesOffsetAfter: 400,
+          centeredSlides: true,
+        },
+      },
+    });
+
     swiper.on('click', (swiper, event) => {
       let currentSlide = swiper.slides[swiper.clickedIndex];
       let newSlide = document.createElement('div');
       newSlide.classList.add('swiper-slide');
       newSlide.dataset.dynamic = true; // Add custom attribute to new slides
 
+      // Remove 'open' data attribute from all slides
+      swiper.slides.forEach((slide) => {
+        delete slide.dataset.open;
+      });
+
       if (previousActiveSlideIndex === undefined) {
         currentSlide.dataset.open = true;
         swiper.addSlide(swiper.clickedIndex + 1, newSlide);
-        //swiper.slideTo(swiper.clickedIndex + 1); // Navigate to the newly added slide
+        swiper.slideTo(swiper.clickedIndex + 1); // Navigate to the newly added slide
       } else if (previousActiveSlideIndex === swiper.clickedIndex) {
-        if (currentSlide.dataset.open) {
-          delete currentSlide.dataset.open;
-          if (swiper.slides[swiper.clickedIndex + 1].dataset.dynamic) {
-            // Check for custom attribute before removing
-            swiper.removeSlide(swiper.clickedIndex + 1);
-          }
+        if (swiper.slides[swiper.clickedIndex + 1].dataset.dynamic) {
+          // Check for custom attribute before removing
+          swiper.removeSlide(swiper.clickedIndex + 1);
         } else {
           currentSlide.dataset.open = true;
           swiper.addSlide(swiper.clickedIndex + 1, newSlide);
-          //swiper.slideTo(swiper.clickedIndex + 1); // Navigate to the newly added slide
+          swiper.slideTo(swiper.clickedIndex + 1); // Navigate to the newly added slide
         }
       } else {
-        delete swiper.slides[previousActiveSlideIndex].dataset.open;
         if (
           swiper.slides[previousActiveSlideIndex + 1] &&
           swiper.slides[previousActiveSlideIndex + 1].dataset.dynamic
@@ -83,7 +114,7 @@ export const initSwipers = () => {
         }
         currentSlide.dataset.open = true;
         swiper.addSlide(swiper.clickedIndex + 1, newSlide);
-        //swiper.slideTo(swiper.clickedIndex + 1); // Navigate to the newly added slide
+        swiper.slideTo(swiper.clickedIndex + 1); // Navigate to the newly added slide
       }
 
       previousActiveSlideIndex = swiper.clickedIndex;
