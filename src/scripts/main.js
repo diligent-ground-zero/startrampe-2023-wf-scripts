3636f72cac0ac9f78e9d12a6a665ef7e19c2f1fd
Niko Dovidija <niko@diligentstudios.com>
2024-01-08 22:00:29 +0100

    edits

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,7 @@
 import '../styles/global.css';
 
 import { globalScripts } from './global';
+import { initAnimations, initSwipers } from './home';
 
 document.addEventListener('DOMContentLoaded', () => {
   globalScripts();
