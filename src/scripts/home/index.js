aa70378d1d2f71329eae9f69bddb44dc988b1d56
Niko Dovidija <niko@diligentstudios.com>
2024-01-22 17:54:14 +0100

    update

--- src/scripts/home/index.js
+++ src/scripts/home/index.js
@@ -235,6 +235,6 @@ export const initAnimations = () => {
       ];
       timeline(sequence, { easingAndDuration, delay: 0.5 });
     },
-    { margin: '0px 0px 0px 0px', amount: 0.35 },
+    { margin: '0px 0px 0px 0px', amount: 0.2 },
   );
 };
